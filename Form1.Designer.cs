c6d90328f9cab5a5032e728826c2f615b7637d72
isnafatchiyah <isnafatchiyah05@gmail.com>
2024-05-22 09:03:47 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace tokorotianyar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnhitung = new System.Windows.Forms.Button();
+            this.btnbatal = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtharga = new System.Windows.Forms.TextBox();
+            this.txtjumlah = new System.Windows.Forms.TextBox();
+            this.txttotal1 = new System.Windows.Forms.TextBox();
+            this.txtkembalian = new System.Windows.Forms.TextBox();
+            this.txtpotongan = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toko Roti Dan Kue ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Roti :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(115, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Harga :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jumlah :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(484, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Harga :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(496, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kembalian :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(62, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Jenis Makanan :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(486, 256);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 18);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total Bayar :";
+            // 
+            // btnhitung
+            // 
+            this.btnhitung.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnhitung.Location = new System.Drawing.Point(93, 341);
+            this.btnhitung.Name = "btnhitung";
+            this.btnhitung.Size = new System.Drawing.Size(75, 30);
+            this.btnhitung.TabIndex = 15;
+            this.btnhitung.Text = "Hitung";
+            this.btnhitung.UseVisualStyleBackColor = true;
+            this.btnhitung.Click += new System.EventHandler(this.btnhitung_Click);
+            // 
+            // btnbatal
+            // 
+            this.btnbatal.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbatal.Location = new System.Drawing.Point(636, 341);
+            this.btnbatal.Name = "btnbatal";
+            this.btnbatal.Size = new System.Drawing.Size(75, 30);
+            this.btnbatal.TabIndex = 16;
+            this.btnbatal.Text = "Bayar";
+            this.btnbatal.UseVisualStyleBackColor = true;
+            this.btnbatal.Click += new System.EventHandler(this.btnbatal_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(506, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Roti Ciabatta",
+            "Roti Focaccia",
+            "Roti Pumpernickel"});
+            this.comboBox1.Location = new System.Drawing.Point(184, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.cmbjenis_SelectedIndexChanged);
+            // 
+            // txtharga
+            // 
+            this.txtharga.Location = new System.Drawing.Point(184, 183);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(121, 20);
+            this.txtharga.TabIndex = 21;
+            // 
+            // txtjumlah
+            // 
+            this.txtjumlah.Location = new System.Drawing.Point(184, 235);
+            this.txtjumlah.Name = "txtjumlah";
+            this.txtjumlah.Size = new System.Drawing.Size(121, 20);
+            this.txtjumlah.TabIndex = 22;
+            // 
+            // txttotal1
+            // 
+            this.txttotal1.Location = new System.Drawing.Point(590, 217);
+            this.txttotal1.Name = "txttotal1";
+            this.txttotal1.Size = new System.Drawing.Size(121, 20);
+            this.txttotal1.TabIndex = 26;
+            // 
+            // txtkembalian
+            // 
+            this.txtkembalian.Location = new System.Drawing.Point(590, 295);
+            this.txtkembalian.Name = "txtkembalian";
+            this.txtkembalian.Size = new System.Drawing.Size(121, 20);
+            this.txtkembalian.TabIndex = 27;
+            // 
+            // txtpotongan
+            // 
+            this.txtpotongan.Location = new System.Drawing.Point(590, 256);
+            this.txtpotongan.Name = "txtpotongan";
+            this.txtpotongan.Size = new System.Drawing.Size(121, 20);
+            this.txtpotongan.TabIndex = 28;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(771, 442);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtpotongan);
+            this.Controls.Add(this.txtkembalian);
+            this.Controls.Add(this.txttotal1);
+            this.Controls.Add(this.txtjumlah);
+            this.Controls.Add(this.txtharga);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnbatal);
+            this.Controls.Add(this.btnhitung);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnhitung;
+        private System.Windows.Forms.Button btnbatal;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtharga;
+        private System.Windows.Forms.TextBox txtjumlah;
+        private System.Windows.Forms.TextBox txttotal1;
+        private System.Windows.Forms.TextBox txtkembalian;
+        private System.Windows.Forms.TextBox txtpotongan;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tokorotianyar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cmbjenis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Roti Ciabatta")
+            {
+                textBox1.Text = "Ciabatta";
+                txtharga.Text = "45000";
+            }
+            else if (comboBox1.SelectedItem == "Roti Focaccia")
+            {
+                textBox1.Text = "Focaccia";
+                txtharga.Text = "50000";
+            }
+            else if (comboBox1.SelectedItem == "Roti Pumpernickel")
+            {
+                textBox1.Text = "Pumpernickel";
+                txtharga.Text = "70000";
+            }
+        }
+
+        private void btnhitung_Click(object sender, EventArgs e)
+        {
+            double tyy, ggd, hde;
+
+            tyy = double.Parse(txtjumlah.Text);
+            ggd = double.Parse(txtharga.Text);
+
+            hde = ggd * tyy;
+
+            txttotal1.Text = hde.ToString();
+
+
+
+
+
+
+        }
+
+        private void btnbatal_Click(object sender, EventArgs e)
+        {
+            double bhf, hdf, dyh;
+
+            bhf = double.Parse(txttotal1.Text);
+            hdf = double.Parse(txtpotongan.Text);
+
+            dyh = hdf - bhf;
+
+            txtkembalian.Text = dyh.ToString();
+        }
+    }
+}
